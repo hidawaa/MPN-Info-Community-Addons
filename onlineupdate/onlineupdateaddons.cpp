7a48dcae2c11ef7532c643535d065afdd1c2d244
Ichdyan Thalasa <ichdyan.thalasa@gmail.com>
2020-09-24 12:11:43 +0700

    cleanup, implement check onlineupdate

---
+++ onlineupdate/onlineupdateaddons.cpp
@@ -0,0 +1,6 @@
+#include "onlineupdateaddons.h"
+
+OnlineUpdateAddons::OnlineUpdateAddons()
+{
+
+}
---
+++ onlineupdate/onlineupdateaddons.h
@@ -0,0 +1,11 @@
+#ifndef ONLINEUPDATEADDONS_H
+#define ONLINEUPDATEADDONS_H
+
+
+class OnlineUpdateAddons
+{
+public:
+    OnlineUpdateAddons();
+};
+
+#endif // ONLINEUPDATEADDONS_H
